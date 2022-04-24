7458b23e2d4c5406e0bcc3a71db9d26f5b8cc87f
Endi Jean <endijean91@gmail.com>
2022-04-24 20:28:03 -0300

    Primeiro commit

---
+++ InitializrSpringbootProjectLabore/src/main/java/com/unifcv/labore/LaboreApplication.java
@@ -0,0 +1,13 @@
+package com.unifcv.labore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboreApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProjectLabore/src/test/java/com/unifcv/labore/LaboreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifcv.labore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
